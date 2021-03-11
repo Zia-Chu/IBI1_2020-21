87f0e4ba4a1e3d884b06c22f1c33deaeb537be9a
Zia Chu <3200115294@zju.edu.cn>
2021-03-11 21:00:27 +0800

    fibonacci.py

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -1,3 +1,11 @@
+#Name the first two number and assign them 1
+#The third number is the sum of the first two
+#Regard the second number as the first number
+#How many times changed the first number?
+#	if less than 12: Keep changing
+#	if 12: Done	
+
+
 a,b=1,1
 for i in range(12):
 	a,b =b,a+b
