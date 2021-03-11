57c56425db000e735d61fae961bd6781fce37038
Zia Chu <3200115294@zju.edu.cn>
2021-03-11 20:32:33 +0800

    variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,21 @@
+a=190102
+b=190784
+c=100321
+d=a-c
+e=b-a
+print(d,e)
+#compare d and e
+if d > e:
+	print (d)
+else:
+	print (e)
+X=True
+Y=False
+Z=(X and not Y)or(Y and not X)
+print(Z)
+W=X!=Y
+print(W)
+
+
+
+
