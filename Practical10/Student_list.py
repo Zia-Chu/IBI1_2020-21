dde3b4faa63756e3b95061d06f37f3ce60cee75b
Zia-Chu <80030886+Zia-Chu@users.noreply.github.com>
2021-05-19 23:41:47 +0800

    Update Student_list.py

--- Practical10/Student_list.py
+++ Practical10/Student_list.py
@@ -1,10 +1,13 @@
 class Student():
+# initialise the arguments
     def __init__(self,first_name,last_name,undergraduate_programme):
+# get the value of arguments
         self.first_name=first_name
         self.last_name=last_name
         self.undergraduate_programme=undergraduate_programme
-
+#print full name and undergraduate programme
     def information(self):
-        print(self.first_name.title()+' '+self.last_name.title()+': '+self.undergraduate_programme)
+        print(self.first_name.title()+' '+self.last_name.title()+': '+self.undergraduate_programme) # turn the name in capitals
+# example        
 student=Student('zihan','chu','BMS')
 student.information()
