4aa685631e11acc8147393051a155f87d0f0b52d
Zia-Chu <80030886+Zia-Chu@users.noreply.github.com>
2021-05-19 23:39:01 +0800

    Update Reverse_complement_calculator.py

--- Practical10/Reverse_complement_calculator.py
+++ Practical10/Reverse_complement_calculator.py
@@ -1,4 +1,6 @@
+# create a function to make complement sequence
 def reverse_complement(sequence):
+# store the complement methods in a dictionary
     complement_table = {
         "A":"T",
         "T":"A",
@@ -9,10 +11,12 @@ def reverse_complement(sequence):
         "g":"c",
         "c":"g",
     }
+# extend complement sequence and return as a string
     sequence_list = list(sequence)
     sequence_list = [complement_table[base] for base in sequence_list]
     string = ''.join(sequence_list)
     return string
+# example
 seqs=reverse_complement('ACTGactgAACCTTGG')
 print(seqs)
 
