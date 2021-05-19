3c542268bb586f4f6a8bd3db48277760d92b7943
Zia-Chu <80030886+Zia-Chu@users.noreply.github.com>
2021-05-19 23:49:09 +0800

    Update GoTree.py

--- practical14/GoTree.py
+++ practical14/GoTree.py
@@ -1,6 +1,5 @@
-#try to use recursion to solve the problem
 import re
-#load an XML document and create a minidom object using the xml.dom
+#load an XML document and create a minidom object
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import os
@@ -8,10 +7,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 os.chdir('C:\cygwin64\home\wjw\Practical14')
 go_obo=open('go_obo.xml','r')
-#use dom because it can form a tree structure
+#use dom to form a tree structure
 DOMTree=xml.dom.minidom.parse('go_obo.xml')
+# define the document element
 obo=DOMTree.documentElement
-#get the term from each sequence
+#get the term element
 terms=obo.getElementsByTagName('term')
 DNA_number=0
 RNA_number=0
@@ -20,7 +20,7 @@ Carbohydrate_number=0
 i=0
 #create a list longer than 2000000
 loc=[0]*3000000
-#create a function which can found the childnode and parentnode
+#create a function to find the childnodes or parentnodes
 def find(j,term):
     is_a = []
     flag = False
@@ -32,14 +32,14 @@ def find(j,term):
             parentid = a.childNodes[0].data
 #get the parentid
             s = re.findall(':(\d.*)$',parentid)
-#change the parentid into numbers
+#turn the parentid into numbers
             digitid = int(s[0])
-#use these numbers to locate the fatherterm
+#locate the fatherterm
             fatherterm = terms[loc[digitid]]
-#get the defstr from the fatherterm
+#find defstr
             defstr = fatherterm.getElementsByTagName('defstr')[0]
             d = defstr.childNodes[0].data
-#find XXX associated gene and continue searching
+#find specific types of gene by text contents
             if re.search(j,d):
                 flag = True
             elif find(j,fatherterm):
@@ -53,7 +53,7 @@ for term in terms:
  q=int(p[0])
  loc[q]=i
  i=i+1
-#use find function to count the number
+#count the number
 #get the number of childnodes associated with DNA
 for term in terms:
  if find('DNA',term):
@@ -69,8 +69,8 @@ for term in terms:
  if find('protein',term):
   Protein_number+=1
 
-#I chose carbohydrate as the fourth macromolecule
-#get the number of childnodes associated with CH
+#choose carbohydrate as the fourth macromolecule
+#get the number of childnodes associated with it
 for term in terms:
  if find('carbohydrate',term):
   Carbohydrate_number+=1
@@ -80,7 +80,7 @@ print('The Number of ChildNodes Associated with RNA:',RNA_number)
 print('The Number of ChildNodes Associated with Protein:',Protein_number)
 print('The Number of ChildNodes Associated with Carbohydrate:',Carbohydrate_number)
 
-#draw a pie chart related to these four numbers
+#draw a pie chart showing these four numbers
 labels='DNA-associated\n'+str(DNA_number),'RNA-associated\n'+str(RNA_number),'Protein-associated\n'+str(Protein_number),'Carbohydrate-associated\n'+str(Carbohydrate_number)
 plt.pie([DNA_number,RNA_number,Protein_number,Carbohydrate_number],explode=None,labels=labels,colors=('y','r','b','c'),autopct='%1.2f%%',pctdistance=0.7,shadow=True, labeldistance=1.0,startangle=0,radius=1.2,counterclock=False,wedgeprops=None,textprops=None,center=(0,0),frame=False)
 plt.title('Numbers of ChildNodes')
